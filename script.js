1bc6e6c0319e7322d75e9880f318631db6ad72b4
Alfredo Hernandez <ahernandez@veradat.com>
2023-11-18 11:53:14 -0600

    pacientes y preguntas

--- script.js
+++ script.js
@@ -1,15 +1,92 @@
 console.log('hola')
 
+function agregarPaciente(listaPacientes, nuevoPaciente) {
+  console.log('lista original', listaPacientes)
+  const nuevaListaPaciente = [...listaPacientes]
+  console.log('nueva lista', nuevaListaPaciente)
 
-function calcularArea(longitud, ancho) {
-  if(typeof longitud !== 'number' || typeof ancho !== 'number' || longitud < 0 || ancho < 0) {
-    console.error('Los parámetros deben ser números no negativos.')
+  const pacienteExistente = nuevaListaPaciente.find((paciente) => paciente.nombre.toLowerCase() === nuevoPaciente.nombre.toLowerCase() )
+  console.log('existe?', pacienteExistente)
+
+  if(pacienteExistente) {
+    console.error(`El paciente "${nuevoPaciente.nombre}" ya existe en la lista.`)
+    return
   }
-  const area = (longitud * ancho).toFixed(2)
-  return parseFloat(area)
+
+  nuevaListaPaciente.push(nuevoPaciente)
+
+  return nuevaListaPaciente
 }
 
-const areaRectangulo = calcularArea(5.67, 8.43)
-console.log(areaRectangulo)
+function buscarPaciente(listaPacientes, nombrePaciente) {
+  if(listaPacientes.length === 0) {
+    return ('La lista de pacientes está vacía.')
+  }
 
-calcularArea(5.67, '8.43')
+  const pacienteEncontrado = listaPacientes.find(
+    paciente => paciente.nombre === nombrePaciente
+  )
+
+  return pacienteEncontrado || `Paciente "${nombrePaciente}" no encontrado.`
+
+}
+
+
+const listaOriginal = [
+  { nombre: 'Paciente1', edad: 30, enfermedad: 'Resfriado' },
+  { nombre: 'Paciente2', edad: 25, enfermedad: 'Dolor de cabeza' },
+  { nombre: 'Paciente3', edad: 35, enfermedad: 'Fiebre' }
+];
+
+const nuevoPaciente = { nombre: 'NuevoPaciente', edad: 28, enfermedad: 'Esguince' }
+
+const listaConNuevoPaciente = agregarPaciente(listaOriginal, nuevoPaciente);
+
+// console.log('Lista con nuevo paciente:', listaConNuevoPaciente)
+
+const nombreBuscar = 'Paciente4'
+const pacienteEncontrado = buscarPaciente(listaOriginal, nombreBuscar)
+
+// console.log('',pacienteEncontrado)
+
+
+const preguntas = [
+  {
+    pregunta: "Cuando hibernan los osos?",
+    opciones: ['invierno', 'verano', 'primavera', 'otoño'],
+    respuestaCorrecta: 'invierno'
+  },
+  {
+    pregunta: "Cuando hibernan los osos?",
+    opciones: ['invierno', 'verano', 'primavera', 'otoño'],
+    respuestaCorrecta: 'invierno'
+  },
+  {
+    pregunta: "Cuando hibernan los osos?",
+    opciones: ['invierno', 'verano', 'primavera', 'otoño'],
+    respuestaCorrecta: 'invierno'
+  },
+  {
+    pregunta: "Cuando hibernan los osos?",
+    opciones: ['invierno', 'verano', 'primavera', 'otoño'],
+    respuestaCorrecta: 'invierno'
+  }
+]
+
+const respuestas = [
+  'invierno', 'otoño', 'verano', ''
+]
+
+let respuestaUsuario = 'invierno' 
+
+const correcto = preguntas.map((pregunta, indice) => {
+  console.log('pregunta', pregunta)
+  console.log('indice', indice)
+  if(pregunta.respuestaCorrecta === respuestas[indice]) {
+    return 'correcto'
+  } else {
+    return 'incorrecto'
+  }
+})
+
+console.log(correcto)
